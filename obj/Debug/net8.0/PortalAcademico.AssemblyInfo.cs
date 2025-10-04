c103903ee9bdeb8ac03bc796f9758c7e870d55d5
axolstv <hchoqqueccota@gmail.com>
2025-10-04 16:17:07 -0500

    Pregunta 2: catálogo de cursos con filtros y vista detalle

--- obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d55810873481d5a8b85520bc125af83b77c310b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e77152d79b9c8afe2f241411672e02c1281bf3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
